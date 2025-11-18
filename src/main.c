cdeadff439cabb24e2b32cb5d6a4039f500665e6
tarnaldo88 <tarnaldo88@gmail.com>
2025-11-18 13:33:16 -0800

    finished while loop for cli

--- src/main.c
+++ src/main.c
@@ -15,9 +15,34 @@ int main() {
         printf("switch> ");
         scanf("%s", command);
 
-        if(!strcmp(command, "exit")) break;
+        if(!strcmp(command, "exit")) { 
+            break;
+        } else if(!strcmp(command, "show")) {
+            switch_show(&sw);
+        }
+        else if (!strcmp(command, "vlan-add")) {
+            int vid;
+            scanf("%d", &vid);
+            switch_create_vlan(&sw, vid);
+        }
+        else if (!strcmp(command, "vlan-del")) {
+            int vid;
+            scanf("%d", &vid);
+            switch_delete_vlan(&sw, vid);
+        }
+        else if (!strcmp(command, "port-add")) {
+            char name[32];
+            scanf("%s", name);
+            switch_add_port(&sw, name);
+        }
+        else if (!strcmp(command, "assign")) {
+            char name[32];
+            int vid;
+            scanf("%s %d", name, &vid);
+            switch_assign_port(&sw, name, vid);
+        }
 
-        
+
     }
     return 0;
 }
