7567b8aef574eb91da20b8c22436978a51c40794
tarnaldo88 <tarnaldo88@gmail.com>
2025-11-18 13:01:25 -0800

    switch_delete_vlan function implemented.

--- src/switch.c
+++ src/switch.c
@@ -13,3 +13,19 @@ bool switch_create_vlan(Switch *sw, int vlan_id) {
 
     return sw->backend->apply_vlan(vlan_id);
 }
+
+bool switch_delete_vlan(Switch *sw, int vlan_id) {
+    for(int i = 0; i < sw->vlan_count; i++) {
+        if(sw->vlans[i].id == vlan_id) {
+            //shift the array
+            for(int j = i; j < sw->vlan_count - 1; j++) {
+                sw->vlans[j] = sw->vlans[j+1];
+            }
+
+            sw->vlan_count--;
+
+            return sw->backend->remove_vlan(vlan_id);
+        }
+    }
+    return false;
+}
