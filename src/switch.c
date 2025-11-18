e9359c7bc146634fd9b4a9b69ae8d0c34fa6d389
tarnaldo88 <tarnaldo88@gmail.com>
2025-11-18 12:57:39 -0800

    switch_create_vlan function implemented

--- src/switch.c
+++ src/switch.c
@@ -5,3 +5,11 @@ void switch_init(Switch * sw, Backend *b) {
     sw->port_count = 0;
     sw->backend = b;
 }
+
+bool switch_create_vlan(Switch *sw, int vlan_id) {
+    if(sw->vlan_count >= MAX_VLANS) return false;
+
+    sw->vlans[sw->vlan_count++].id = vlan_id;
+
+    return sw->backend->apply_vlan(vlan_id);
+}
