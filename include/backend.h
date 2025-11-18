1f3981f4acda225079eaa2885e1f5c1243b39100
tarnaldo88 <tarnaldo88@gmail.com>
2025-11-18 12:44:22 -0800

    finished backend header file

--- include/backend.h
+++ include/backend.h
@@ -5,4 +5,8 @@
 
 typedef struct Backend {
     bool (*apply_vlan)(int vlan_id);
-}
+    bool (*remove_vlan)(int vlan_id);
+    bool (*assign_port_to_vlan)(const char *port, int vlan_id);
+} Backend;
+
+#endif
